764978a5ce9c62b3233748b7e48cfadb29c8f771
Olkusz Michał <olkusz.michal@gmail.com>
2024-03-11 17:54:00 +0100

    poprawa corsów

--- controllers/userController.go
+++ controllers/userController.go
@@ -119,9 +119,8 @@ func Login(c *gin.Context) {
 		})
 		return
 	}
-
-	c.SetSameSite(http.SameSiteLaxMode)
-	c.SetCookie("Authorization", tokenString, 3600*24*30, "", "", false, true)
+	c.SetSameSite(http.SameSiteNoneMode)
+	c.SetCookie("Authorization", tokenString, 3600*24*30, "/", "localhost", true, true)
 
 	c.JSON(http.StatusOK, gin.H{})
 }
